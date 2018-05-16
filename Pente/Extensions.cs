a656c5475bb265181254a8eb3c9c800a68954215
Unknown <CRobinson@student.neumont.edu>
2018-05-16 10:59:13 -0600

    Add Extension Methods
    Added Piece[].GetPatternFor(Piece)
    Added Piece.Mult(Piece)

---
+++ Pente/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente
+{
+    static class Extensions
+    {
+        public static Board.Piece[] GetPatternFor(this Board.Piece[] pattern, Board.Piece playerPiece)
+        {
+            Board.Piece[] newPattern = new Board.Piece[pattern.Length];
+            pattern.CopyTo(newPattern, 0);
+
+            for (int i = 0; i < newPattern.Length; i++)
+            {
+                newPattern[i] = newPattern[i].Mult(playerPiece);
+            }
+
+            return newPattern;
+        }
+
+        public static Board.Piece Mult(this Board.Piece left, Board.Piece right)
+        {
+            return (Board.Piece)((int)left * (int)right);
+        }
+    }
+}
