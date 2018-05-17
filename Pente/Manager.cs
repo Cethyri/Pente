d57f92cceabdae5f89eed38898f9b2239fa36ed8
Unknown <WEller@student.neumont.edu>
2018-05-17 11:43:51 -0600

    Menu Navigation complete
    Mode selection window is complete and leads to the board

---
+++ Pente/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pente
+{
+    class Manager
+    {
+        public static Manager instance;
+
+        public Player p1;
+        public Player p2;
+         
+        public Board board;
+
+        private Manager()
+        {
+            instance = this;
+            p1 = new Player();
+            p2 = new Player();
+            board = new Board();
+        }
+
+        public static void CreateNewManager()
+        {
+            if (instance == null)
+            {
+                Manager manager = new Manager();
+            }
+        }
+
+    }
+}
