a2ae32b5bf01fb52526f3c073162600bcbe39cc0
Zachery Anderson <zanderson@student.neumont.edu>
2018-05-17 09:50:53 -0600

    Bugs bugs bugs. Window aren't doing what they are supposed to be doing at the moment

---
+++ Pente/GameWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pente
+{
+	/// <summary>
+	/// Interaction logic for GameWindow.xaml
+	/// </summary>
+	public partial class GameWindow : Window
+	{
+		Window parent;
+		public GameWindow(Window window)
+		{
+			parent = window;
+			InitializeComponent();
+		}
+		~GameWindow()
+		{
+			parent.Visibility = Visibility.Visible;
+		}
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			e.Cancel = true;
+		}
+	}
+}
--- Pente/MainWindow.xaml.cs
+++ Pente/MainWindow.xaml.cs
@@ -20,20 +20,49 @@ namespace Pente
     /// </summary>
     public partial class MainWindow : Window
     {
+		HowToPlay howToPlayWindow;
+		GameWindow gameWindow;
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+
+		}
+
+		~MainWindow()
+		{
+			if(howToPlayWindow != null)
+			howToPlayWindow.Close();
+
+		}
+			
         //PopupWindow for mode, Select mode, true/false for mode select, popup name window, open actual game window
         public void Start()
         {
 
-        }
+		}
 
-        public void Quit()
+		public void Quit()
         {
             Application.Current.Shutdown();
         }
-    }
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			gameWindow = new GameWindow(this);
+			this.Hide();
+			gameWindow.Show();
+		}
+
+		private void btnHowToPlay_Click(object sender, RoutedEventArgs e)
+		{
+			howToPlayWindow = new HowToPlay();
+			howToPlayWindow.Show();
+		}
+
+		private void btnExit_Click(object sender, RoutedEventArgs e)
+		{
+			Quit();
+		}
+	}
 }
