242c05206ed9148a7553d2e4d666d8cf15cd3502
Unknown <WEller@student.neumont.edu>
2018-05-18 11:52:12 -0600

    Zach is a monster
    The entire project was reformatted to account for the fact that we are only using one window

---
+++ Pente/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pente
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        //	HowToPlay howToPlayWindow;
+        public MainPage()
+        {
+            InitializeComponent();
+            Manager.CreateNewManager();
+
+        }
+
+
+
+        public void NewGame()
+        {
+       //     gameWindow = new GameWindow(this);
+        //    gameWindow.Show();
+        }
+
+        //PopupWindow for mode, Select mode, true/false for mode select, popup name window, open actual game window
+        public void Start()
+        {
+            //Do we still need this?
+        }
+
+        public void Quit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/ModePage.xaml", UriKind.Relative));
+            //gameWindow = new GameWindow(this);
+            //this.Hide();
+            //gameWindow.Show();
+
+          //  modeSelect = new ModeSelect(this);
+           // this.Hide();
+       //     modeSelect.Show();
+
+        }
+
+        private void btnHowToPlay_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.pente.net/instructions.html");
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Quit();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("THIS ISNT IMPLEMENTED YET!");
+        }
+    }
+}
